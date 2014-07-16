37d984f5a600ddd0bffc4ba9367d399d66c8a0ec
Rachit Gupta <rachit.gupta@jabong.com>
2014-07-16 15:19:34 +0530

    Created basic sign in page.

--- index.php
+++ index.php
@@ -1,10 +1,40 @@
 <!DOCTYPE html>
 <head>
     <title>PHP Hands On App</title>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <?php
-        echo "Hello PHP";
-    ?>
+    <div id="container">
+        <h1 class="col-sm-offset-2">Sign In!</h1>
+        <br />
+        <form class="form-horizontal" role="form">
+            <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                <div class="col-sm-3">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                <div class="col-sm-3">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
+            </div>
+       </form>
+       <br />
+       <h2 class="col-sm-offset-2">Don't have an account?</h2>
+       <br />
+       <div class="form-group">
+                <div class="col-sm-offset-2">
+                   <input type="button" class="btn btn-primary" value="Create account"/>
+               </div>
+        </div>
+   </div>
 </body>
 </html>
