4ef4a1a25507794aa4847713d7bd1485b4d2c3c2
Akshat Rustagi <akshat.rustagi@jabong.com>
2014-07-17 14:23:45 +0530

    Added details of creation of account to database

--- createAccount.php
+++ createAccount.php
@@ -16,21 +16,48 @@
         }
     }
     </script>
+    <?php
+        include('./dbHandler.php');
+        $dbHandler = new Db_Handler("localhost", "root", "test");
+        $emailID = isset($_POST['userEmail']) ? $_POST['userEmail'] : '';
+        $passwd = isset($_POST['passWord']) ? $_POST['passWord'] : '';
+        $fname = isset($_POST['firstName']) ? $_POST['firstName'] : '';
+        $lname = isset($_POST['lastName']) ? $_POST['lastName'] : '';
+        if($emailID == '')
+        {
+            //do nothing
+        }
+        else
+        {  
+            $dbHandler->openConnection();
+                if($dbHandler->checkEmailExists($emailID))
+                {
+                    echo "Email already exists, choose another one";
+                }
+                else
+                {
+                    if($dbHandler->createEntry($emailID,$passwd,$fname,$lname))
+                        echo "Accout Created Successfully";
+                    else
+                        echo "Something Went Wrong";
+                }
+        }               
+    ?>
 </head>
 <body>
     <h1 class="text-center">Create Account</h1>
     <br /> 
     <h3 class="col-sm-offset-2">Account Details</h3>
     <br />
-    <form  class="form-horizontal" role="form">
+    <form  class="form-horizontal" role="form" action="<?php $_PHP_SELF?>" method="POST">
         <div class="form-group">
             <div class="col-sm-offset-2 col-sm-3">
-                <input type="email" class="form-control" id="inputEmail" placeholder="Enter email" required />
+                <input type="email" class="form-control" id="inputEmail" name="userEmail" placeholder="Enter email" required />
             </div>
         </div>
         <div class="form-group">
             <div class="col-sm-offset-2 col-sm-3">
-                <input type="password" class="form-control" id="password" placeholder="Enter password" required />
+                <input type="password" class="form-control" id="password" name="passWord" placeholder="Enter password" required />
             </div>
         </div>
         <div class="form-group">
@@ -42,12 +69,12 @@
     <br />
         <div class="form-group">
             <div class="col-sm-offset-2 col-sm-3">
-                <input type="text" class="form-control" id="inputName" placeholder="Enter First Name" required>
+                <input type="text" class="form-control" id="inputName" name="firstName" placeholder="Enter First Name" required>
             </div>
         </div>
         <div class="form-group">
             <div class="col-sm-offset-2 col-sm-3">
-                <input type="text" class="form-control" id="inputLastName" placeholder="Enter Last Name" required>
+                <input type="text" class="form-control" id="inputLastName" name="lastName" placeholder="Enter Last Name" required>
             </div>
         </div>
     <div class="form-group">
